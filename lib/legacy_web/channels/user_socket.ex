c3bdad1bd747d925f8f65ee52498b98a4be2ba85
Maz Jaleel <mazjaleel@gmail.com>
2018-08-23 00:41:30 +0400

    Generate Lobby and Game channels

--- lib/legacy_web/channels/user_socket.ex
+++ lib/legacy_web/channels/user_socket.ex
@@ -2,11 +2,11 @@ defmodule LegacyWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", LegacyWeb.RoomChannel
+  channel "lobby:*", LegacyWeb.LobbyChannel
+  channel "game:*", LegacyWeb.GameChannel
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
-  # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
